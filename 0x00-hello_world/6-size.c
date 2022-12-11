e4afae5deba7008fbc8f9b4012d8ec7278ce38ee
preciousmiracle605 <micphynzboe123@gmail.com>
2022-12-11 03:22:18 -0800

    printing various type size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print  size of various types on the computer
+ *
+ * Return:0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of char = %lu bytes\n", sizeof(a));
+	printf("size of int = %lu bytes\n", sizeof(b));
+	printf("size of long int = %lu bytes\n", sizeof(c));
+	printf("size of long long int = %lu bytes\n", sizeof(d));
+	printf("size of float = %lu bytes\n", sizeof(e));
+	return (0);
+}
