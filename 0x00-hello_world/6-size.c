e71626a9445a3482bc851af81fc39d5eceda641b
preciousmiracle605 <micphynzboe123@gmail.com>
2022-12-11 06:50:55 -0800

    coding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - print  size of various types on the computer
  *
- * Return:0
+ * Return: 0
 */
 int main(void)
 {
@@ -12,10 +12,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of char = %lu bytes\n", sizeof(a));
-	printf("size of int = %lu bytes\n", sizeof(b));
-	printf("size of long int = %lu bytes\n", sizeof(c));
-	printf("size of long long int = %lu bytes\n", sizeof(d));
-	printf("size of float = %lu bytes\n", sizeof(e));
+	printf("size of char: %lu bytes\n", (unsigned long)sizeof(a));
+	printf("size of int: %lu bytes\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu bytes\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu bytes\n", (unsigned long)sizeof(d));
+	printf("size of float: %lu bytes\n", (unsigned long)sizeof(e));
 	return (0);
 }
