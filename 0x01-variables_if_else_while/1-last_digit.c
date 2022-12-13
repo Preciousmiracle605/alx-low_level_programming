6ba452721affce28d21f62dbb23f8eea29a03d59
preciousmiracle605 <micphynzboe123@gmail.com>
2022-12-12 23:12:25 -0800

    finding x

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,10 +21,10 @@ int main(void)
 	}
 	if (x == 0)
 	{
-		printf("and is zero");
+		printf(" and is zero");
 	}
 	if (x < 6 && x != 0)
-		printf("and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0");
 	printf("\n");
 	return (0);
 }
